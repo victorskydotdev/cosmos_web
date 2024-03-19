bc44b62020231c29f5722395a3b8bbfc464f4796
techyVS <victorsky90@gmail.com>
2024-03-19 11:51:36 +0100

    added an about us page

--- src/assets/js/menu-interaction.js
+++ src/assets/js/menu-interaction.js
@@ -1,15 +1,19 @@
 // variables
 
 const mobileMenu = document.querySelector('.nav-links');
-const burgerButton = document.querySelector('.hamburger');
+const burgerButton = document.querySelector('.open-btn');
+const closeBtn = document.querySelector('.close-btn');
 
 export const showHideNav = () => {
 	burgerButton.addEventListener('click', () => {
 		mobileMenu.classList.add('show-nav');
+		burgerButton.style.display = 'none';
+		closeBtn.style.display = 'block';
+	});
 
-		// hide the burger btn
-		if (burgerButton) {
-			burgerButton.style.display = 'none';
-		}
+	closeBtn.addEventListener('click', () => {
+		mobileMenu.classList.remove('show-nav');
+		burgerButton.style.display = 'block';
+		closeBtn.style.display = 'none';
 	});
 };
